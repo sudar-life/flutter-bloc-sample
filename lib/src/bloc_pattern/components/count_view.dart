aa9687a440ee06230269030402da1500113c8d4c
sudar-life <mousai86@gmail.com>
2021-01-07 16:29:29 +0900

    bloc dispose 처리

--- lib/src/bloc_pattern/components/count_view.dart
+++ lib/src/bloc_pattern/components/count_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import '../../../main.dart';
+import 'package:flutter_bloc_sample/src/bloc_pattern/ui/bloc_display_widget.dart';
 
 class CountView extends StatelessWidget {
   CountView({Key key}) : super(key: key);
@@ -14,7 +13,10 @@ class CountView extends StatelessWidget {
         initialData: 0,
         builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
           if (snapshot.hasData) {
-            return Text(snapshot.data.toString());
+            return Text(
+              snapshot.data.toString(),
+              style: TextStyle(fontSize: 80),
+            );
           }
           return CircularProgressIndicator();
         },
--- lib/src/bloc_pattern/ui/bloc_display_widget.dart
+++ lib/src/bloc_pattern/ui/bloc_display_widget.dart
@@ -2,12 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc_sample/src/bloc_pattern/bloc/count_bloc.dart';
 import 'package:flutter_bloc_sample/src/bloc_pattern/components/count_view.dart';
 
-import '../../../main.dart';
+CountBloc countBloc;
 
-class BlocDisplayWidget extends StatelessWidget {
-  CountEventBloc _countEventBloc;
-  BlocDisplayWidget() {
-    _countEventBloc = countBloc.countEventBloc;
+class BlocDisplayWidget extends StatefulWidget {
+  @override
+  _BlocDisplayWidgetState createState() => _BlocDisplayWidgetState();
+}
+
+class _BlocDisplayWidgetState extends State<BlocDisplayWidget> {
+  @override
+  void initState() {
+    super.initState();
+    countBloc = CountBloc();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    countBloc.dispose();
   }
 
   @override
@@ -23,13 +35,14 @@ class BlocDisplayWidget extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.add),
             onPressed: () {
-              _countEventBloc.countEventSink.add(CountEvent.ADD_EVENT);
+              countBloc.countEventBloc.countEventSink.add(CountEvent.ADD_EVENT);
             },
           ),
           IconButton(
             icon: Icon(Icons.remove),
             onPressed: () {
-              _countEventBloc.countEventSink.add(CountEvent.SUBTRACT_EVENT);
+              countBloc.countEventBloc.countEventSink
+                  .add(CountEvent.SUBTRACT_EVENT);
             },
           ),
         ],
